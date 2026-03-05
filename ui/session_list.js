8ac6f8fede7b8f4d9457a95c3cce2602dca72f4f
Adrian Wannenmacher <tfld@tfld.dev>
2026-03-05 02:51:51 +0100

    redesign session list

--- ui/session_list.js
+++ ui/session_list.js
@@ -5,33 +5,36 @@ import Session from "/models/session.js";
 export default class SessionList {
   /** @param {{ attrs: { models: Session[] } }} param The sessions to show. */
   view({attrs: { models, onSelect } }) {
-    return m("section", [
-      m(m.route.Link, {
-        href: "/",
-        selector: "button.wb-button",
-        options: {
-          state: { newSession: true },
-        },
-      }, "Neia Satz"),
+    return m("section.wb-session-list", [
       m("ol", [
-        models.map((s) => m("li", [
-          m(
-            m.route.Link,
-            {
-              href: "/",
-              selector: "button.wb-button",
-              params: { session: s.id },
-              onclick: () => onSelect(s),
-            },
-            [
-              m("p", "•".repeat(s.result.theirPoints)),
-              m("p", s.theirTeam !== "" ? s.theirTeam : "Se"),
-              m("p", s.ourTeam !== "" ? s.ourTeam : "Mia"),
-              m("p", "•".repeat(s.result.ourPoints)),
-            ],
+        models.map((s) => m("li.item._alternate._apply", [
+          m("span.theirname", s.theirTeam !== "" ? s.theirTeam : "Se"),
+          m("span.ourname", s.ourTeam !== "" ? s.ourTeam : "Mia"),
+          m("span.theirpoints", "•".repeat(s.result.theirPoints)),
+          m("span.ourpoints", "•".repeat(s.result.ourPoints)),
+          m("div.actions",
+            m(
+              m.route.Link,
+              {
+                href: "/",
+                selector: "button.wb-button.-slim._positioned",
+                params: { session: s.id },
+                onclick: () => onSelect(s),
+              },
+              "spieln",
+            ),
           ),
-        ]))
-      ])
+        ])),
+      ]),
+      m("footer.wb-box._sticky-bottom._alternate._apply",
+        m(m.route.Link, {
+          href: "/",
+          selector: "button.wb-button._positioned",
+          options: {
+            state: { newSession: true },
+          },
+        }, "Neia Satz"),
+      ),
     ]);
   }
 }
