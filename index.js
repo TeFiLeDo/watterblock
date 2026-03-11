a5e8ec0a07080d7c4e66bdb836916696623d4729
Adrian Wannenmacher <tfld@tfld.dev>
2026-03-11 19:06:36 +0100

    disable service worker registration
    A recent bug has proven that the service worker cache refresh does not
    yet work as intended and can lead to a bug where the entire app does not
    load. It therefore has been disabled for now.

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ import BaseView from "/ui/base_view.js";
 import InfoView from "/ui/info_view.js";
 import Layout from "/ui/layout.js";
 
-try {
-  navigator.serviceWorker.register("/service-worker.js", { scope: "/" });
-} catch (error) {
-  console.error("failed to register service worker: ", error);
-}
+// try {
+//   navigator.serviceWorker.register("/service-worker.js", { scope: "/" });
+// } catch (error) {
+//   console.error("failed to register service worker: ", error);
+// }
 
 m.route.prefix = "";
 m.route(document.body, "/", {
